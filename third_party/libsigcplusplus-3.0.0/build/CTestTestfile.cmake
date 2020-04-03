af954130407e7fdcd6a86904145d2f2df8fe723d
Aart <aart_odding@hotmail.com>
2020-04-03 16:58:27 +0200

    removed third party dependencies

--- third_party/libsigcplusplus-3.0.0/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aart/Documents/libsigcplusplus-3.0.0
-# Build directory: /Users/aart/Documents/libsigcplusplus-3.0.0/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("sigc++")
-subdirs("examples")
-subdirs("tests")
--- third_party/libsigcplusplus-3.0.0/build/sigc++/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aart/Documents/libsigcplusplus-3.0.0/sigc++
-# Build directory: /Users/aart/Documents/libsigcplusplus-3.0.0/build/sigc++
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
